a7707c54fa53841124612c0ac1aafb848732a0b0
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2023-04-09 11:40:07 +0200

    add partner and styles

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_03_142757) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_09_070236) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "communiques", force: :cascade do |t|
     t.string "titre"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "titre"
+    t.string "email"
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
