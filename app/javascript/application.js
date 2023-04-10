274b838c9c1c88c8d3826b4ee3052fdd4c9081d8
kandy <kandypeter03@gmail.com>
2023-04-10 16:41:24 +0200

    fix turbo links issue

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,8 +6,16 @@ import "controllers"
 //= require jquery
 //= require slick
 
-$(document).ready(function() {
-  $('.slider').slick({
+
+import "@hotwired/turbo-rails"
+import "controllers"
+
+//= require jquery
+//= require slick
+
+
+function initializeSlider() {
+  $('.slider:not(.slick-initialized)').slick({
     autoplay: true,
     autoplaySpeed: 3000,
     arrows: true,
@@ -15,20 +23,59 @@ $(document).ready(function() {
     prevArrow: '<button type="button" class="prev"> << </button>',
     nextArrow: '<button type="button" class="next"> >> </button>'
   });
-});
+}
 
-
-$(document).ready(function() {
-  $('.popup-btn').click(function() {
+function initializePopup() {
+  $('body').on('click', '.popup-btn', function() {
     $(this).siblings('.popup').fadeIn();
   });
 
-  $('.popup').click(function(e) {
+  $('body').on('click', '.popup', function(e) {
     if (e.target == this) {
       $(this).fadeOut();
     }
   });
+}
+
+function initialize() {
+  initializeSlider();
+  initializePopup();
+}
+
+
+document.addEventListener("turbo:load", function() {
+  initialize();
 });
+
+// $(document).on('turbolinks:load', function () {
+
+//   initialize();
+// });
+
+
+// $(document).ready(function() {
+//   $('.slider').slick({
+//     autoplay: true,
+//     autoplaySpeed: 3000,
+//     arrows: true,
+//     dots: false,
+//     prevArrow: '<button type="button" class="prev"> << </button>',
+//     nextArrow: '<button type="button" class="next"> >> </button>'
+//   });
+// });
+
+
+// $(document).ready(function() {
+//   $('.popup-btn').click(function() {
+//     $(this).siblings('.popup').fadeIn();
+//   });
+
+//   $('.popup').click(function(e) {
+//     if (e.target == this) {
+//       $(this).fadeOut();
+//     }
+//   });
+// });
 
 
 
