93ea3df835807b89434e80c32f146526f7c66a48
kandy <kandypeter03@gmail.com>
2023-04-14 16:47:35 +0200

    add first user to be admin

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -26,8 +26,16 @@ class Users::SessionsController < Devise::SessionsController
   # end
   protected
 
+  # def after_sign_in_path_for(resource)
+  #   if current_user.is_admin?
+  #     admin_home_index_path
+  #   else
+  #     root_path
+  #   end
+  # end
+
   def after_sign_in_path_for(resource)
-    if current_user.is_admin?
+    if resource.is_admin?
       admin_home_index_path
     else
       root_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
+  def self.first_user?
+    count == 1
+  end
+
+
+  after_create :set_first_user_as_admin
+
+  private
+
+  def set_first_user_as_admin
+    if User.first_user?
+      update(is_admin: true)
+    end
+  end
 end
