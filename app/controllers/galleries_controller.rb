1573b0dc0355de4389efba27ae151e3de53c7711
Yannick <yannickzahinda442@gmail.com>
2024-09-13 10:29:47 +0200

    add better error handling for image upload

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,26 +1,31 @@
 class GalleriesController < ApplicationController
-    def index
-      @galleries = Gallery.all
-    end 
-    
-    def new
-      @gallery = Gallery.new
-    end
+  def index
+    @galleries = Gallery.all
+  end 
   
-    def create
-      @gallery = Gallery.new(gallery_params)
+  def new
+    @gallery = Gallery.new
+  end
+
+  def create
+    @gallery = Gallery.new(gallery_params)
+    begin 
       if @gallery.save
         ImageUploaderJob.perform_async(@gallery.id)
         redirect_to galleries_path, notice: 'Image upload is in progress'
       else
         render :new
       end
-    end
-  
-    private
-  
-    def gallery_params
-      params.require(:gallery).permit(:image)
+    rescue Faraday::TimeoutError
+      @gallery.errors.add(:base, "Request time out. Please try again later")
+      render :new
     end
   end
+
+  private
+
+  def gallery_params
+    params.require(:gallery).permit(:image)
+  end
+end
   
